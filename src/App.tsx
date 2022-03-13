7400aa3f26bc24d99067b409ea1d89c2ec525c7c
unknown <dlord4242@gmail.com>
2022-03-13 14:08:06 +0900

    Chagned sample src

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import VideoPlayer from './components/Player/VideoPlayer';
 import './App.css';
 
-// const src = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4';
+// const src = 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4';
 
 function App() {
   return (
